e700991c2c363a9d4287e7dd11e651cb9d7cf4c7
abhitech2002 <oabhishek31@gmail.com>
2025-01-19 20:56:29 +0530

    add basic Express server setup with dotenv configuration

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+    });
+
+app.listen(process.env.PORT, () => {    
+    console.log(`URL Shortner app listening on port ${process.env.PORT}`);
+});
