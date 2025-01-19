d0015bbcaa7e348ea73815747d0a7e97aa6c7fd2
abhitech2002 <oabhishek31@gmail.com>
2025-01-19 21:09:36 +0530

    add MongoDB connection setup and integrate with Express server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 dotenv.config();
 const app = express();
@@ -8,8 +9,13 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World');
-    });
+});
 
-app.listen(process.env.PORT, () => {    
-    console.log(`URL Shortner app listening on port ${process.env.PORT}`);
+connectDB().then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`URL Shortner app listening on port ${process.env.PORT}`);
+    });
+}).catch((err) => {
+    console.error('Error connecting to the database:', err);
+    process.exit(1);
 });
