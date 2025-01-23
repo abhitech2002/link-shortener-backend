8f9575664bc5e69c035d6c56c67b2b9da0a9a4e1
abhitech2002 <oabhishek31@gmail.com>
2025-01-23 09:27:05 +0530

    add testing support with Jest and Supertest; update MongoDB connection for test environment

--- config/db.js
+++ config/db.js
@@ -1,14 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+const connectDB = async (isTest = false) => {
+  const uri = isTest ? `${process.env.MONGO_URI}-test` : process.env.MONGO_URI;
+  try {
+    const conn = await mongoose.connect(uri);
 
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
-    }
+    console.log(
+      `MongoDB connected (${isTest ? "Test" : "Production"}): ${
+        conn.connection.host
+      }`
+    );
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
